a5c7ea4beea22dbb5b7c21dc143c3fd365db2060
Владислав <ivanov.vladislav.yurevich@gmail.com>
2025-06-26 18:45:30 +0500

    add

--- app/js/main.js
+++ app/js/main.js
@@ -3,3 +3,24 @@ let mainTrustSwiper = new Swiper(".main-trust__swiper", {
     spaceBetween: 20,
     loop: true,
 });
+
+
+
+const progressCircle = document.querySelector('.ring-progress');
+const radius = 46;
+const circumference = 2 * Math.PI * radius;
+
+progressCircle.style.strokeDasharray = `${circumference}`;
+progressCircle.style.strokeDashoffset = `${circumference}`;
+
+function setProgress(percent) {
+  const offset = circumference - (percent / 100) * circumference;
+  progressCircle.style.strokeDashoffset = offset;
+}
+
+window.addEventListener('scroll', () => {
+  const scrollTop = window.scrollY;
+  const docHeight = document.body.scrollHeight - window.innerHeight;
+  const scrollPercent = (scrollTop / docHeight) * 100;
+  setProgress(scrollPercent);
+});
